c4905923e7aebe923a300d7d1280db6b94b14b99
galloppinggryphon <26875847+galloppinggryphon@users.noreply.github.com>
2021-10-12 09:45:01 -0400

    Better comments.

--- examples/config.js
+++ examples/config.js
@@ -1,31 +1,34 @@
 exports.config = {
-	// Block namespace: all block names must begin with a prefix, e.g. myblock:stairs_granite. This must be unique, but short. Do not include a colon. For more info, see
+	// # Block namespace #
+	// All block names must begin with a prefix, e.g. myblock:stairs_granite
+	// Prevent block name conflicts with other addons.
+	// The namespace must be unique, but short. Do not include a colon.
 	prefix: 'hubgen',
 
-	// Input options
+	// # Input options #
 	// Configure template file locations
 	// All template files can be relocated to a subfolder, e.g. presets: 'config/presets.json',
 	input: {
-		// List of block permutation definition files (array). Can also be supplied with command line arguments
+		// = List of block permutation definition files (array). Can also be supplied with command line arguments =
 		// All template files must begin with 'blocks-'
 		// Use blocks-*.json to automatically include all files that begin with 'blocks-'. Other wildcards are not supported.
 		// Example: [ 'blocks-roofs.json', 'blocks-walls.json',  'blocks-vslabs.json' ]
 		blocks: [ 'blocks-*.json' ],
 
-		// Collection of code snippets that can be included in blocks
+		// = Collection of code snippets that can be included in blocks =
 		presets: 'presets.json',
 
-		// Scaffolding for all block files
+		// = Scaffolding for all block files =
 		scaffolding: 'scaffolding.json',
 	},
 
-	// Output options
-	// Configure output options
+	// # Output options #
+	// Configure block naming and output paths
 	output: {
-		// Output directory, relative to root
+		// = Output directory, relative to root =
 		outputDir: 'output',
 
-		// Name and title separators
+		// = Name and title separators =
 		// Configure separators between different permutation segments
 		// Define how permutation names and titles should be put together
 		// Makes reading block lists easier
@@ -33,15 +36,24 @@ exports.config = {
 		// Type can be any string, except '*' and 'material'
 		// Name separator constrains => valid: [ .()-_ ], invalid: [  space,+,; ]
 		// Example output: vslab.centred--width_medium(stonebrick)
-		nameSeparators: { '*': '.', size: '--', material: [ '(', ')' ] },
-		titleSeparators: { '*': ' - ', material: [ ' (', ') ' ] },
+		nameSeparators: {
+			'*': '.',
+			size: '--',
+			material: [ '(', ')' ],
+		},
+		titleSeparators: {
+			'*': ' - ',
+			material: [ ' (', ') ' ],
+		},
 
-		// Create directory structure for block files based on X segments of the block permutation path (nesting levels)
-		// E.g. [pathSegmentation: 2]
+		// = Directory structure for block files =
+		// Sort block files by creating folder for segments of the permutation path (nesting levels) =
+		// Choose the number of segments to use.
+		// E.g. the first two segments: [pathSegmentation: 2]
 		// vslab.thick.align_centre.acacia ==> vslab/thick/align_centre_acacia.json
 		pathSegmentation: 4,
 
-		// Save generated block titles to this translation file
+		// = Save generated block titles to this translation file =
 		language: 'en_US',
 	},
 
