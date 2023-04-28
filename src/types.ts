f4e1190a93b0aed7b5eef85eb089485a0745ade0
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-04-28 12:12:41 -0400

    Formatting

--- src/types.ts
+++ src/types.ts
@@ -458,14 +458,14 @@ interface ExtraVars {
 
 
 type LogItem = {
-	level: number,
-	levelStr: string,
-	context: string,
-	label: string,
-	msg: string,
-	line?: string,
-	file?: string,
-	column?: string,
-	stacktrace?: string,
-	additionalData: string
-}
+	level: number;
+	levelStr: string;
+	context: string;
+	label: string;
+	msg: string;
+	line?: string;
+	file?: string;
+	column?: string;
+	stacktrace?: string;
+	additionalData: string;
+};
