850cc4c3d5564553c3623f9805d0be3a034f2fe6
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-08-25 17:25:57 -0400

    Refactored. Added stacktrace.

--- src/builder/generator-log.js
+++ src/builder/generator-log.js
@@ -1,32 +1,35 @@
 import path from 'path'
-
 import chalk from 'chalk'
 import _ from 'lodash'
 
-// function log( ...msg ) {
-// 	console.log( ...msg )
-// }
-
 const log = console.log.bind( console )
+
+const NL = '\n'
+
+const logLevels = /** @type {const} */ ( {
+	error: 1,
+	warn: 2,
+	notice: 3,
+} )
+
+const stats = {
+	errors: 0,
+	warnings: 0,
+	notices: 0,
+}
+
+const logLevelLabels = {
+	error: chalk.redBright.bold( 'ERROR' ),
+	warn: chalk.yellowBright.bold( 'WARNING' ),
+	notice: chalk.cyan.bold( 'NOTICE' ),
+}
 
 /**
  * Logging factory.
  */
 export default function GeneratorLog() {
-	const logLevels = /** @type {const} */ ( {
-		error: 1,
-		warn: 2,
-		notice: 3,
-	} )
-
-	const stats = {
-		errors: 0,
-		warnings: 0,
-		notices: 0,
-	}
-
 	/**
-	 * @type {{[store: string]: LogItem[]}}
+	 * @type {{[store: string]: ReturnType<typeof LogMsg>[]}}
 	 */
 	const stack = {}
 	const current = { store: '', label: '', context: undefined }
@@ -41,6 +44,18 @@ export default function GeneratorLog() {
 				|| ( threshold === 2 && ( stats.warnings || stats.errors ) )
 				|| ( threshold === 3 && ( stats.notices || stats.warnings || stats.errors ) )
 		},
+
+		addMsg( level, message, errorData = undefined, stacktrace = undefined ) {
+			const item = LogMsg()
+			item.create( level, message, errorData, stacktrace )
+
+			const store = current.store || 'default'
+			stack[ store ] = stack[ store ] ?? []
+			stack[ store ].push( item )
+
+			return item
+		},
+
 		/** @throws Throws error if called. */
 		fatal( message, ...errorData ) {
 			log( '\n\n' )
@@ -52,27 +67,32 @@ export default function GeneratorLog() {
 				this.printLogItems()
 			}
 
-			const item = NewLogItem( 'error', message, errorData )
-
 			log()
 			log( chalk.yellow( 'Fatal error:' ) )
-			log( ...formatLogItem( item ) )
+
+			const item = this.addMsg( 'error', message, errorData )
+			const parts = item.format( true )
+			parts.forEach( ( i ) => log( i ) )
 
 			throw new Error( 'FATAL ERROR! See error info above.' )
 		},
-		error( message, ...errorData ) {
+		error( message, errorData, stacktrace = undefined ) {
 			stats.errors++
-			return NewLogItem( 'error', message, errorData )
+
+			const item = this.addMsg( 'error', message, errorData, stacktrace )
+			return item.data
 		},
-		warn( message, ...errorData ) {
+		warn( message, errorData, stacktrace = undefined ) {
 			stats.warnings++
-			return NewLogItem( 'warn', message, errorData )
+			const item = this.addMsg( 'warn', message, errorData, stacktrace )
+			return item.data
 		},
-		notice( message, ...errorData ) {
+		notice( message, errorData, stacktrace = undefined ) {
 			stats.notices++
-			return NewLogItem( 'notice', message, errorData )
+			const item = this.addMsg( 'notice', message, errorData, stacktrace )
+			return item.data
 		},
-		printLog( threshold = 3 ) {
+		printLog( { verbose = false } = {} ) {
 			const logStack = Object.entries( stack )
 
 			if ( ! logStack.length ) {
@@ -81,19 +101,17 @@ export default function GeneratorLog() {
 
 			if ( stats.errors ) {
 				log()
-				// log( chalk.bold( chalk.bgRedBright( ' WARNING ' ) + chalk.bgWhite( ' Errors occurred during processing! ' ) ) )
-				// log()
-				log( chalk.yellow( 'Problems occurred during processing of one or more files, blocks or block variants (permutations)\n\nBlock output may contain invalid syntax or be incomplete!' ) )
+				log( chalk.yellow( 'Problems occurred during processing of one or more files, blocks or block variants (permutations)\n\nBlock output may be incomplete or contain invalid syntax!' ) )
 			}
 			else if ( stats.warnings ) {
 				log( chalk.yellow( 'Warnings were generated.' ) )
 			}
 
 			log()
-			this.printLogItems( logStack )
+			this.printLogItems( logStack, verbose )
 		},
 
-		printLogItems( logItems = undefined ) {
+		printLogItems( logItems = undefined, verbose = false ) {
 			const logStack = logItems ?? Object.entries( stack )
 
 			if ( ! logStack.length ) {
@@ -115,7 +133,8 @@ export default function GeneratorLog() {
 				}
 
 				items.forEach( ( item ) => {
-					log( ...formatLogItem( item ) )
+					const parts = item.format( verbose )
+					parts.forEach( ( i ) => log( i ) )
 				} )
 			} )
 		},
@@ -135,80 +154,110 @@ export default function GeneratorLog() {
 
 	return logHandler
 
-	/**
-	 *
-	 * @param {keyof typeof logLevels} level
-	 * @param {string} msg
-	 * @param {*} additionalData
-	 */
-	function NewLogItem( level, msg, additionalData ) {
-		let resolvedAdditionalData = additionalData && additionalData.length === 1 ? additionalData[ 0 ] : additionalData
-		resolvedAdditionalData = resolvedAdditionalData !== undefined && ( Object( resolvedAdditionalData ) === resolvedAdditionalData ? _.cloneDeep( resolvedAdditionalData ) : resolvedAdditionalData )
-
-		const levelInt = logLevels[ level ]
-		const { file, line, column } = getLocation()
-
+	function LogMsg() {
 		/** @type {LogItem} */
-		const logItem = {
-			level: levelInt,
-			levelStr: level,
-			context: current.context,
-			label: current.label,
-			msg,
-			additionalData: resolvedAdditionalData,
-			file,
-			line,
-			column,
+		let _logItem
+
+		/**
+		* @param {keyof typeof logLevels} level
+		* @param {string} message
+		* @param {*} additionalData
+		* @param {Error} errorObj
+		*/
+		function create( level, message, additionalData, errorObj = undefined ) {
+			const resolvedAdditionalData = additionalData === undefined ? undefined : ( Object( additionalData ) === additionalData ? _.cloneDeep( additionalData ) : additionalData )
+
+			const levelInt = logLevels[ level ]
+			const stacktrace = getStackTrace( errorObj )
+			const { file, line, column } = getLocation( stacktrace )
+
+			_logItem = {
+				level: levelInt,
+				levelStr: level,
+				context: current.context,
+				label: current.label,
+				msg: message,
+				additionalData: resolvedAdditionalData,
+				file,
+				line,
+				column,
+				stacktrace: stacktrace.join( NL ),
+			}
+
+			return _logItem
 		}
 
-		const store = current.store || 'default'
-		stack[ store ] = stack[ store ] ?? []
-		stack[ store ].push( logItem )
-		return logItem
-	}
+		/**
+		 * @param {boolean} verbose
+		 */
+		function format( verbose ) {
+			const { additionalData, column, context, file, label, levelStr, line, msg, stacktrace } = _logItem
+			const _label = label && context === logHandler.BLOCK_BUILDER ? chalk.yellowBright( `[${ label }]\n` ) : ''
+			const location = chalk.cyanBright( `@${ file }:${ line }:${ column }` )
+			const item = [ `${ _label }${ logLevelLabels[ levelStr ] }  >>>  ${ msg }\n${ location }` ]
 
-	/**
-	 * @param {LogItem} stackItem
-	 */
-	function formatLogItem( stackItem ) {
-		const logLevelLabels = {
-			error: chalk.redBright.bold( 'ERROR' ),
-			warn: chalk.yellowBright.bold( 'WARNING' ),
-			notice: chalk.cyan.bold( 'NOTICE' ),
+			if ( additionalData !== undefined ) {
+				const additionalDataString = JSON.stringify( additionalData, jsonReplaceUndefined, 4 )
+
+				// !Bug: without an empty array item, the arrow below is not displayed!
+				item.push( '' )
+				item.push( chalk.dim( additionalDataString ), '' )
+			}
+
+			if ( verbose ) {
+				item.push( `${ chalk.yellow( '◆ ◆ ◆  Stack Trace  ◆ ◆ ◆' ) }` )
+				item.push( chalk.dim( stacktrace ) )
+			}
+
+			item.push( NL )
+
+			return item
 		}
 
-		const { additionalData, column, context, file, label, levelStr, line, msg } = stackItem
-		const _label = label && context === logHandler.BLOCK_BUILDER ? chalk.yellowBright( `${ label }\n` ) : ''
+		function getStackTrace( errorObj = undefined ) {
+			const _errorObj = errorObj ?? new Error()
+			const errorMsg = `Error: ${ _errorObj.message }`
+			let stacktrace = _errorObj.stack
+			stacktrace = stacktrace.replace( new RegExp( errorMsg, 'g' ), '' )
 
-		// const item = [ `${ label }${ logLevelLabels[ stackItem.levelStr ] } @{}> ${ stackItem.msg }` ]
-		const location = chalk.cyanBright( `@${ file }:${ line }:${ column }` )
-		const item = [ `${ _label }${ logLevelLabels[ levelStr ] }  >>>  ${ msg }\n${ location }` ]
-
-		if ( additionalData ) {
-			// ▣▶◆◇↪
-			item.push( '\n↪ ' )
-			item.push( additionalData )
+			// Split by line, discard 5 first lines
+			const lines = stacktrace.split( NL )
+			return errorObj ? lines.slice( 1 ) : lines.slice( 5 )
 		}
 
-		item.push( '\n' )
+		/**
+		 * @param {string[]} stacktrace
+		 */
+		function getLocation( stacktrace ) {
+			// Grab first line, were logger was called)
+			const string = stacktrace[ 0 ]
 
-		return item
+			// Parse string to get file and line
+			const rx = /(file:\/\/\/(?<file>.*):(?<line>[\d]+):(?<column>[\d]+))/i
+			const info = string.match( rx )
+
+			if ( ! info || ! info.groups ) {
+				return { file: 'n/a' }
+			}
+
+			const { file, line, column } = info.groups
+
+			// Show path relative to cwd
+			const cwd = process.cwd()
+			const __file = path.relative( cwd, file )
+			return { file: __file, line, column }
+		}
+
+		return {
+			create,
+			get data() {
+				return _logItem
+			},
+			format,
+		}
 	}
+}
 
-	function getLocation() {
-		const stacktrace = new Error().stack
-
-		// Split by line, then grab line 5 (line were logger was called)
-		const string = stacktrace.split( '\n' )[ 4 ]
-
-		// Parse string to get file and line
-		const rx = /(file:\/\/\/(?<file>.*):(?<line>[\d]+):(?<column>[\d]+))/i
-		const info = string.match( rx )
-		const { file, line, column } = info.groups
-
-		// Show path relative to cwd
-		const cwd = process.cwd()
-		const __file = path.relative( cwd, file )
-		return { file: __file, line, column }
-	}
+function jsonReplaceUndefined( __, value ) {
+	return typeof value === 'undefined' ? '<undefined>' : value
 }
